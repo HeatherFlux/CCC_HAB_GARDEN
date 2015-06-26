1e8f3a460cda0af0a7b96e2b500fa1e925d66a17
Heather <h.c.nelson1@gmail.com>
2015-06-26 12:59:39 -0500

    Update CCC_HAB_Garden.ino

--- CCC_HAB_Garden.ino
+++ CCC_HAB_Garden.ino
@@ -7,7 +7,11 @@ char server[] = "api.pushingbox.com"; //pushingbox API server
 IPAddress ip(192,168,1,8); //Arduino IP address. Only used when DHCP is turned off.
 EthernetClient client; //define 'client' as object
 String data;
-String humdata;
+String humiData;
+String soilData;
+String tempfData;
+
+float soil = 0;
 float temp;
 float tempf;
 float humi;
@@ -19,24 +23,35 @@ boolean connecting = false;
   DHT dht(DHTPIN, DHTTYPE);
 
 
+// -------------------------------------------------------------------------------------------------------
+// put your setup code here, to run once:
 void setup() {
-  // put your setup code here, to run once:
- Serial.begin(9600);
- dht.begin();
- if (Ethernet.begin(mac) == 0) {
-	Serial.println("Failed to configure Ethernet using DHCP");
-	Ethernet.begin(mac, ip);
+     Serial.begin(9600);
+     dht.begin();
+       if (Ethernet.begin(mac) == 0) {
+	  Serial.println("Failed to configure Ethernet using DHCP");
+	  Ethernet.begin(mac, ip);
   }
   delay(1000);
 }
 
-void loop() {
-  // read your sensors
-  temp = dht.readTemperature();
-  tempf = dht.readTemperature(true);
-  humi = dht.readHumidity();
+// -------------------------------------------------------------------------------------------------------
+//begin the loops
+  void loop() {
   
-  //begin the loops
+// read your sensors
+   temp = dht.readTemperature();
+   tempf = dht.readTemperature(true);
+   humi = dht.readHumidity();
+   int sensorValue = analogRead(A0);
+       sensorValue = constrain(sensorValue, 485, 1023);
+       soil = map(sensorValue, 485, 1023, 100, 0);  //map the value to a percentage
+  
+
+
+//
+// -------------------------- Timestamp Temperature in Celsius 
+//
    getdatatemp();
    Serial.println("connecting...");
    if (client.connect(server, 80)) {
@@ -54,16 +69,20 @@ void loop() {
     if (!client.connected()) {
 	  Serial.println();
 	  Serial.println("disconnecting.");
-          Serial.print("Temperature Sent :");
+          Serial.print("Temperature Sent : ");
           Serial.print(temp); //print sent value to serial monitor
-          Serial.print(","); //print sent value to serial monitor
-          Serial.print(tempf); //print sent value to serial monitor
 	  client.stop(); 
           connecting = false; 
           data = ""; //data reset
     }
   }
-  delay(100);
+  delay(1000);
+ 
+//
+// -------------------------- Humidity
+//
+ 
+  Serial.println(" ");
   Serial.println("Getting/Sending Humidity");
      getdatahum();
    Serial.println("connecting...");
@@ -83,21 +102,83 @@ void loop() {
 	  Serial.println();
 	  Serial.println("disconnecting.");
           Serial.print("Humidity Sent :");
-          Serial.print(humi); //print sent value to serial monitor
+          Serial.println(humi); //print sent value to serial monitor
 	  client.stop(); 
           connecting = false; 
-          humdata = ""; //data reset
+          humiData = ""; //data reset
     }
   }
-  delay(600000); // interval
+    delay(1000);
+
+//
+// -------------------------- Soil
+//
+    
+  Serial.println(" ");
+  Serial.println("Getting/Sending Soil Moisture Level");
+     getdatasoil();
+   Serial.println("connecting...");
+   if (client.connect(server, 80)) {
+     sendDatasoil();  
+     connecting = true; //connected = true
+   }
+   else{
+     Serial.println("connection failed");
+   }
+    while(connecting){
+    if (client.available()) {
+	  char c = client.read(); //save http header to c
+	  Serial.print(c); //print http header to serial monitor
+    }
+    if (!client.connected()) {
+	  Serial.println();
+	  Serial.println("disconnecting.");
+          Serial.print("Soil Moisture Level Sent :");
+          Serial.println(soil); //print sent value to serial monitor
+	  client.stop(); 
+          connecting = false; 
+          soilData = ""; //data reset
+    }
+  }
+    delay(1000);
+//
+// -------------------------- Temperature in Fahrenheit 
+//
+  Serial.println(" ");
+  Serial.println("Getting/Sending Temperature(F)");
+     getdatatempf();
+   Serial.println("connecting...");
+   if (client.connect(server, 80)) {
+     sendDatatempf();  
+     connecting = true; //connected = true
+   }
+   else{
+     Serial.println("connection failed");
+   }
+    while(connecting){
+    if (client.available()) {
+	  char c = client.read(); //save http header to c
+	  Serial.print(c); //print http header to serial monitor
+    }
+    if (!client.connected()) {
+	  Serial.println();
+	  Serial.println("disconnecting.");
+          Serial.print("Temperature(F) Sent :");
+          Serial.println(tempf); //print sent value to serial monitor
+	  client.stop(); 
+          connecting = false; 
+          tempfData = ""; //data reset
+    }
+  }
+  delay(60000); // interval
 }
 
+// -------------------------------------------------------------------------------------------------------
+// Temp(C) Functions
 void getdatatemp(){
   data+="";
   data+="GET /pushingbox?devid=vA9E6B4A30C33313&tempData="; //GET request query to pushingbox API
   data+=temp;
-  data+=",";
-  data+=tempf;
   data+=" HTTP/1.1";
 }
 void sendDatatemp(){
@@ -108,15 +189,49 @@ void sendDatatemp(){
   client.println();
 }
 
+// -------------------------------------------------------------------------------------------------------
+// Humidity Functions
 void getdatahum(){
-  humdata+="";
-  humdata+="GET /pushingbox?devid=v599FDEDA89D901C&humiData="; //GET request query to pushingbox API
-  humdata+=humi;
-  humdata+=" HTTP/1.1";
+  humiData+="";
+  humiData+="GET /pushingbox?devid=v599FDEDA89D901C&humiData="; //GET request query to pushingbox API
+  humiData+=humi;
+  humiData+=" HTTP/1.1";
 }
 void sendDatahum(){
   Serial.println("connected");
-  client.println(humdata);
+  client.println(humiData);
+  client.println("Host: api.pushingbox.com");
+  client.println("Connection: close");
+  client.println();
+}
+
+// -------------------------------------------------------------------------------------------------------
+// Soil Moisture Functions
+void getdatasoil(){
+  soilData+="";
+  soilData+="GET /pushingbox?devid=vCEFEE5BDB7082C4&soilData="; //GET request query to pushingbox API
+  soilData+=soil;
+  soilData+=" HTTP/1.1";
+}
+void sendDatasoil(){
+  Serial.println("connected");
+  client.println(soilData);
+  client.println("Host: api.pushingbox.com");
+  client.println("Connection: close");
+  client.println();
+}
+
+// -------------------------------------------------------------------------------------------------------
+// Temp(F) Functions
+void getdatatempf(){
+  tempfData+="";
+  tempfData+="GET /pushingbox?devid=v2362388FBAF17F6&tempfData="; //GET request query to pushingbox API
+  tempfData+=tempf;
+  tempfData+=" HTTP/1.1";
+}
+void sendDatatempf(){
+  Serial.println("connected");
+  client.println(tempfData);
   client.println("Host: api.pushingbox.com");
   client.println("Connection: close");
   client.println();
